8d71d462a31ab339c011ea2b19f79f477c9e5dae
Joao Cassanji <joaogbcassanji98@gmail.com>
2024-11-15 16:02:50 -0300

    test

--- iac/computing.tf
+++ iac/computing.tf
@@ -4,7 +4,7 @@ module "vm" {
   name = var.virtual_machine[count.index].name
   instances = var.virtual_machine[count.index].instances
   size = var.virtual_machine[count.index].size
-  credentials = {username = var.vm_username, password = var.TF_VAR_vm_password}
+  credentials = {username = var.vm_username, password = var.vm_password}
   resource_group_name = module.rg.name
   location = var.location
   subnet_id = module.vnet.subnets[var.virtual_machine[count.index].subnet_name]
